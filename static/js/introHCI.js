317eb1eb6252157c537e1b4bc2ede2909a98eb00
olasubomi awokoya <olasubomi.awokoya@hotmail.com>
2016-01-21 16:45:19 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,11 +8,59 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
+
+/*function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#test.js").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}*/
+
+
+function initializePage() {
+    $("#submitBtn").click(updateProject); 
+} 
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
+
+
+function projectClick(e){
+	console.log("Project clicked");
+	// prevent the page from reloading
+	e.preventDefault();
+
+	// In an event handler, $(this) refers to
+	// the object that triggered the event
+	//$(this).css("background-color", "#7fff00");
+	
+	// In an event listener, $(this) is the element that fired the event
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $("#jumbotron h1");
+
+	jumbotronHeader.text(projectTitle);
+	
+	var containingProject = $(this).closest(".project");
+//containingProject.append("<div class='project-description'<p>Description of the project.</p></div>");
+
+	var description = $(containingProject).find(".project-description");
+	//if (description.length == 0) {
+       	//$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    	//} else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    //}
+	$("#containingProject").fadeToggle("slow")
+       	//$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
 }
